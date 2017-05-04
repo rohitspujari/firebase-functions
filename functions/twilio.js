89e1611c44d678949158161411f3b8a0967890f7
Rohit Pujari <rohitspujari@gmail.com>
2017-05-04 17:31:42 -0500

    fourth commit

--- functions/twilio.js
+++
@@ -1,7 +0,0 @@
-const twilio = require('twilio');
-const credentials = require('./twilio_account.json');
-
-const accountSid = 'AC9757218ec7adb23cc85bf038b21b4a27';
-const authToken =  '3169ae7b14311490b085d5d8c3d4160f'
-
-module.exports = new twilio.Twilio(accountSid, authToken);
